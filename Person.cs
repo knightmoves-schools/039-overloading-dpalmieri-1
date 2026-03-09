dd4877cfc46616aa26a80d7a5e04c94173523edf
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-03-09 12:05:10 -0500

    Refactor Person class and add methods
    Refactored constructors and added Display and Test methods.

--- Person.cs
+++ Person.cs
@@ -6,16 +6,29 @@ public class Person{
    public int Age;
 
    //Add your code here
-   public Constructor1(string firstName, string lastName, int age){
-      firstName = FirstName;
-      lastName = LastName;
-      age = Age;
-   }
    public Constructor1(string firstName, string lastName){
       this.firstName = FirstName;
       this.lastName = LastName;
    }
+    public Constructor1(string firstName, string lastName, int age){
+      firstName = FirstName;
+      lastName = LastName;
+      age = Age;
+   }
+
+   public void Display() {
+      return $"{firstName}, {lastName}, {age}";
+   }
+
+   public static void Test(){
+      Constructor1 test1 = new Constructor1("Alex", " Blex");
+      Constructor1 test2 = new Constructor1("Mua", 22);
+
+      test1.Display();
+      test2.Display();
+   }
 }
+
 
 
 
